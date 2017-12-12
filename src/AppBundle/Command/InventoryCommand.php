03be1745bae56a7c7a668a3349995312b9b71452
Robert Saylor <robert@customphpdesign.com>
2017-12-12 06:44:31 -0500

    added weekly inventory

---
+++ src/AppBundle/Command/InventoryCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InventoryCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:inventory')
+            ->setDescription('This should be ran weekly by a cronjob to create inventory.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $doctrine = $this->getContainer()->get('doctrine');
+        $em = $doctrine->getEntityManager();
+        $base_date = "";
+
+        $sql = "SELECT `inventory_start_date`,`id` FROM `locations` WHERE `auto_inventory` = 'On'";
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute();
+
+        while ($row = $result->fetch()) {   
+            // get last date_code
+            $sql2 = "SELECT `date_code` FROM `inventory` WHERE `locationID` = '$row[id]' ORDER BY `date_code` DESC LIMIT 1";
+            $result2 = $em->getConnection()->prepare($sql2);
+            $result2->execute();
+            while ($row2 = $result2->fetch()) {
+                $base_date = $row2['date_code'];
+            }            
+            $start = date("Ymd", strtotime($base_date));
+            $end = date("Ymd", strtotime($start . "+7 DAY"));
+
+            $output->writeln("Creating inventory: $start to $end");
+            $inventoryResult = $this->getContainer()->get('commonservices')->create_inventory($start,$end);
+            $output->writeln("Done");
+
+        } 
+		//$sql = $this->getContainer()->get('commonservices')->checkin_report_query();
+
+
+    }
+}
--- src/AppBundle/Service/commonservices.php
+++ src/AppBundle/Service/commonservices.php
@@ -222,4 +222,152 @@ class commonservices extends Controller
         return($sql);
     }
 
+    public function create_initial_inventory() {
+        // This will be ran manually with a new lodge
+        $em = $this->em;
+        $container = $this->container;    
+
+        $results = "true";
+
+        $em = $this->getDoctrine()->getManager();
+
+        $bed_map[0] = "A";
+        $bed_map[1] = "B";
+        $bed_map[2] = "C";
+        $bed_map[3] = "D";
+
+        $child_map[0] = "Child1";
+        $child_map[1] = "Child2";
+
+        $sql = "
+        SELECT 
+            `l`.`id`,
+            DATE_FORMAT(`l`.`inventory_start_date`,'%Y%m%d') AS 'start',
+            DATE_FORMAT(`l`.`inventory_stop_date`,'%Y%m%d') AS 'end',
+            `r`.`beds`,
+            `r`.`children`,
+            `r`.`nightly_rate`,
+            `r`.`id` AS 'roomID',
+            `r`.`type` 
+
+        FROM 
+            `locations` l, `rooms` r
+
+        WHERE 
+            1
+            AND `l`.`init` = 'Yes'
+            AND `l`.`id` = `r`.`locationID`
+        ";
+
+
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute();
+        while ($row = $result->fetch()) {
+
+            $start = $row['start'];
+            $end = $row['end'];
+            $next = "";
+
+            while ($next != $end) {
+                    $next = date("Ymd", strtotime($next . "+1 day"));
+
+                    for($x=0; $x < $row['beds']; $x++) {
+                        $sql2 = "INSERT INTO `inventory`
+                        (`locationID`,`reservationID`,`contactID`,`type`,`status`,`roomID`,`typeID`,
+                        `date_code`,`nightly_rate`,`bed`)
+                        VALUES
+                        ('$row[id]','0','0','adult','avail','$row[roomID]','$row[type]',
+                        '$next','$row[nightly_rate]','".$bed_map[$x]."')
+                        ";
+                        $result2 = $em->getConnection()->prepare($sql2);
+                        $result2->execute();                        
+                    }
+                    for($x=0; $x < $row['children']; $x++) {
+                        $sql2 = "INSERT INTO `inventory`
+                        (`locationID`,`reservationID`,`contactID`,`type`,`status`,`roomID`,`typeID`,
+                        `date_code`,`nightly_rate`,`bed`)
+                        VALUES
+                        ('$row[id]','0','0','child','avail','$row[roomID]','$row[type]',
+                        '$next','$row[nightly_rate]','".$child_map[$x]."')
+                        ";  
+                        $result2 = $em->getConnection()->prepare($sql2);
+                        $result2->execute();                          
+                    }
+            }           
+            $sql2 = "UPDATE `locations` SET `init` = 'No' WHERE `id` = '$row[id]'";   
+            $result2 = $em->getConnection()->prepare($sql2);
+            $result2->execute();  
+        }
+        return('done');
+    }
+
+    public function create_inventory($start='',$stop='') {
+        // This will be ran weekly
+        $em = $this->em;
+        $container = $this->container;    
+
+        $results = "true";
+
+        $em = $this->getDoctrine()->getManager();
+
+        $bed_map[0] = "A";
+        $bed_map[1] = "B";
+        $bed_map[2] = "C";
+        $bed_map[3] = "D";
+
+        $child_map[0] = "Child1";
+        $child_map[1] = "Child2";
+
+        $sql = "
+        SELECT 
+            `l`.`id`,
+            `r`.`beds`,
+            `r`.`children`,
+            `r`.`nightly_rate`,
+            `r`.`id` AS 'roomID',
+            `r`.`type` 
+
+        FROM 
+            `locations` l, `rooms` r
+
+        WHERE 
+            1
+            AND `l`.`init` = 'No'
+            AND `l`.`auto_inventory` = 'On'
+            AND `l`.`id` = `r`.`locationID`
+        ";
+
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute();
+        while ($row = $result->fetch()) {
+            $next = $start;
+            while ($next != $stop) {
+                $next = date("Ymd", strtotime($next . "+1 day"));
+                for($x=0; $x < $row['beds']; $x++) {
+                    $sql2 = "INSERT INTO `inventory`
+                    (`locationID`,`reservationID`,`contactID`,`type`,`status`,`roomID`,`typeID`,
+                    `date_code`,`nightly_rate`,`bed`)
+                    VALUES
+                    ('$row[id]','0','0','adult','avail','$row[roomID]','$row[type]',
+                    '$next','$row[nightly_rate]','".$bed_map[$x]."')
+                    ";
+                    $result2 = $em->getConnection()->prepare($sql2);
+                    $result2->execute();                        
+                }
+                for($x=0; $x < $row['children']; $x++) {
+                    $sql2 = "INSERT INTO `inventory`
+                    (`locationID`,`reservationID`,`contactID`,`type`,`status`,`roomID`,`typeID`,
+                    `date_code`,`nightly_rate`,`bed`)
+                    VALUES
+                    ('$row[id]','0','0','child','avail','$row[roomID]','$row[type]',
+                    '$next','$row[nightly_rate]','".$child_map[$x]."')
+                    ";  
+                    $result2 = $em->getConnection()->prepare($sql2);
+                    $result2->execute();                          
+                }
+            }            
+        }
+        return($result);
+    }    
+
 }
